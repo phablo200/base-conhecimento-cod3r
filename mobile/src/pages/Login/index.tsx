e81a9775cb956dbd9d4f102c2325d41f5e795900
Phablo Vilas Boas <phablo_sena@hotmail.com>
2021-07-25 19:47:53 -0300

    construindo página de login

--- mobile/src/pages/Login/index.tsx
+++ mobile/src/pages/Login/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import {FormControl, Stack, Input, Box, Text} from 'native-base';
+import {FormControl, Stack, Input, Box, Text, Button} from 'native-base';
 import {StyleSheet} from 'react-native';
+import { useState } from 'react';
 
 const styles = StyleSheet.create({
     container: {
@@ -18,18 +19,52 @@ const styles = StyleSheet.create({
     input: {
         marginTop: '2%',
     },
+    button: {
+        marginTop: '2%',
+    },
 });
 
 const Login = () => {
+    const [login, setLogin] = useState('phablovilasboas25@gmail.com');
+    const [password, setPassword] = useState('12345');
+    const handleLogin = (value: string) => {
+        setLogin(value);
+    };
+    const handlePassowrd = (value: string) => {
+        setPassword(value);
+    };
+
+    const handleClick = () => {
+        alert('Entrar');
+    };
+
     return (
         <Box style={styles.container}>
             <Box style={styles.boxText}>
                 <Text>Faça seu login</Text>
             </Box>
-            <FormControl isRequired isInvalid>
+            <FormControl isRequired>
                 <Stack>
-                    <Input style={styles.input} placeholder="Login" />
-                    <Input style={styles.input} placeholder="Senha" />
+                    <Input
+                        type="text"
+                        value={login}
+                        style={styles.input}
+                        placeholder="Login"
+                        onChange={e => handleLogin(e.target.value)}
+                    />
+                    <Input
+                        type="password"
+                        value={password}
+                        style={styles.input}
+                        placeholder="Senha"
+                        onChange={e => handlePassowrd(e.target.value)}
+                    />
+                    <Button
+                        style={styles.button}
+                        size="md"
+                        onPress={handleClick}>
+                        Entrar
+                    </Button>
                 </Stack>
             </FormControl>
         </Box>
